dedcccbcd0d755eb48983c5b62f79ee66757ac77
mehkan <mehkanarshadskp@gmail.com>
2023-04-24 20:36:42 +0500

    rails models added

--- db/migrate/20230424145625_create_students.rb
+++ db/migrate/20230424145625_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[7.0]
   def change
     create_table :students do |t|
-      t.string :first_:name
+      t.string :first_name
       t.string :last_name 
       t.string :email
       
