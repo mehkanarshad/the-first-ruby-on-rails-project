8417736cd233d16a7b140d27ac1bc160ac8ea2b1
mehkan <mehkanarshadskp@gmail.com>
2023-05-05 14:52:10 +0500

    time is now

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -24,4 +24,11 @@ class StudentsController < ApplicationController
     def student_params 
         params.require(:student).permit(:first_name,:last_name,:email)
     end
+    def show 
+        @student = Student.find(params[:id])
+    end
+    def edit
+        @student = Student.find(params[:id])
+    end
+
 end
